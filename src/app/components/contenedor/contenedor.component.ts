e7f06b696b3d3d5fccb8882604ffc197f2d96199
curso2024-git <curso202410@gmail.com>
2024-07-31 20:26:44 +0200

    Filtro y Json

--- src/app/components/contenedor/contenedor.component.ts
+++ src/app/components/contenedor/contenedor.component.ts
@@ -2,19 +2,33 @@ import { Component } from '@angular/core';
 import { GestorpeliculasService } from '../../services/gestorpeliculas.service';
 import { PeliculaComponent } from "../pelicula/pelicula.component";
 import { CommonModule } from '@angular/common';
+/* Import para el filtro */
+import { Pelicula } from '../../interfaces/pelicula';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contenedor',
   standalone: true,
-  imports: [PeliculaComponent, CommonModule],
+  imports: [PeliculaComponent, CommonModule, FormsModule],
   templateUrl: './contenedor.component.html',
   styleUrl: './contenedor.component.css'
 })
 export class ContenedorComponent {
+  /* Atributos para filtro */
+  public filtro: string = "";
+  public peliculasFiltradas: Pelicula[] = [];
 
   /* Inyectamos servicios */
   constructor(public gestorPeliculas: GestorpeliculasService) {
+    /* Añadimos filtro -> inicializamos con el array de service */
+    this.peliculasFiltradas = gestorPeliculas.peliculas;
 
+  }
+
+  buscar(): void {
+    this.peliculasFiltradas = this.gestorPeliculas.peliculas.filter(pelicula => {
+      return pelicula.titulo.includes(this.filtro);
+    });
   }
 
 }
--- src/app/services/gestorpeliculas.service.ts
+++ src/app/services/gestorpeliculas.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Pelicula } from '../interfaces/pelicula';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GestorpeliculasService {
+
+  /* Http Headers */
+  static httpHeaders = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+  static URL: string = "https://test2prueba.free.beeceptor.com";
+  static PATH = "/todos";
+
   peliculas: Pelicula[] = [];
 
+  //constructor() { } //inicial
 
-  constructor() { }
+  /* Http Client */
+  constructor(private clienteHttp: HttpClient) {
+    this.clienteHttp.get<Pelicula[]>(`${GestorpeliculasService.URL}${GestorpeliculasService.PATH}`)
+      .forEach(data => {
+        data.forEach(pelicula => {
+          this.peliculas.push(pelicula);
+        });
+      })
+  }
+
   agregar(pelicula: Pelicula): void {
     this.peliculas.push(pelicula);
   }
